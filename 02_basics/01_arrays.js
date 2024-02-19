85dca3b4d6942b2fd4aeceeccf3b01024beff93f
Aniket Dey <102988568+AniketDey06@users.noreply.github.com>
2024-02-19 11:28:19 +0000

    done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+// Array
+
+const myArr = [ 1,3,8,4,33]
+// const myArr2 = [ "ahbxhb","nfvjn","nvsjn"]
+// console.log(myArr);
+// console.log(myArr2);
+
+// const myArr3 = new Array(55,99,6)
+// console.log(myArr3);
+// console.log(myArr3[1]);
+
+// myArr.push(55)
+// myArr.push(99)
+// console.log(myArr);
+// myArr.pop()
+
+// console.log(myArr);
+// myArr.unshift(0)
+// myArr.unshift(33)
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr);
+
+const myArr2 = [ 1,3,8,4,33,76,0,44]
+
+console.log("A",myArr2);
+const mynArr = myArr2.slice(2,5)
+console.log("*",mynArr);
+
+console.log("B",myArr2);
+const mynArr2 = myArr2.splice(2,5)
+console.log("*",mynArr2);
+
+console.log("C",myArr2);
