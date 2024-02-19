7d9d9d58574397e15d7522d5c1ba536ad8a245fa
Aniket Dey <102988568+AniketDey06@users.noreply.github.com>
2024-02-19 14:50:00 +0000

    array2 done

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -12,5 +12,15 @@
 // const myNewArr = [...myArr, ...myArr2, 2]
 // console.log(myNewArr);
 
-const myArr = [ 1,3,[4,5],6,[7,[8,9]]]
-console.log(myArr.flat(Infinity));
+// const myArr = [ 1,3,[4,5],6,[7,[8,9]]]
+// console.log(myArr);
+// console.log(myArr.flat(Infinity));
+
+// console.log(Array.isArray("aniket"));
+// console.log(Array.from("aniket"));
+// console.log(Array.from({name: "aniket"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
