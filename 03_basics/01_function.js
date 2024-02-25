ed0b2a7f04d5ac09e7793f1173bb5edd1cf7432f
Aniket Dey <102988568+AniketDey06@users.noreply.github.com>
2024-02-25 06:01:55 +0000

    fuction part 2 done

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -25,4 +25,33 @@ function loginUserMessage(uname) {
     return `${uname} Just Logged in`
 }
 
-console.log(loginUserMessage("a"));
+// console.log(loginUserMessage("a"));
+
+function cart(...num1) {
+    return num1
+}
+
+// console.log(cart(50,90,55));
+
+const user = {
+    username: "aniket",
+    price: 199
+}
+
+function handleObje(anyobject) {
+    console.log(`User is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObje(user)
+
+// handleObje({
+//     username: "chiku",
+//     price: 399
+// })
+
+const arr = [200, 400, 900]
+function returnVals(anyarray) {
+    return anyarray[2]
+}
+
+// console.log(returnVals(arr));
