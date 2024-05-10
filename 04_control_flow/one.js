157692fd8d0b391efdc05976f0de38e84436fa15
Aniket Dey <meaniket1025@gmail.com>
2024-05-10 16:10:12 +0530

    one done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,4 +1,17 @@
-const temp = 55
-if (temp<70){
-    console.log("it is high");
+// const temp = 55
+// if (temp<70){
+//     console.log("it is high");
+// }
+
+const userLoggedIn = true;
+const HaveMoney = true;
+const loggedInGoogle = false;
+const loggedInLinkedin = true;
+
+if (userLoggedIn && HaveMoney) {
+    console.log("can Shop");
+}
+
+if(loggedInGoogle || loggedInLinkedin){
+    console.log("Can logIn");
 }
