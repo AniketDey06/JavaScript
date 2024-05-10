a16ff348ab450ed54748046d098a5c7390b08919
Aniket Dey <meaniket1025@gmail.com>
2024-05-11 00:18:41 +0530

    loop one done

--- 05_loops/one.js
+++ 05_loops/one.js
@@ -1,6 +1,31 @@
 
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     console.log(index);
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`outer loop: ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`inner loop:${i} ${j}`);
+//     }
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+//     if(i===5){
+//         break
+//     }
+// }
+
+for (let i = 0; i <= 10; i++) {
+    if(i===5){
+        console.log(`in if ${i}`);
+        continue
+    }
+    console.log(i);
 }
+
