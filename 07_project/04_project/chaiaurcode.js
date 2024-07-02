dd25ab5b04b6f6ef3fb16e8b33c09d2e8e8769ea
Aniket Dey <meaniket1025@gmail.com>
2024-07-02 11:50:47 +0530

    project

---
+++ 07_project/04_project/chaiaurcode.js
@@ -0,0 +1,31 @@
+const rnum = Math.round((Math.random()*100)+1)
+console.log(rnum);
+
+const submit = document.querySelector('#subt')
+const guess = document.querySelector('#guessField').value
+const guessSlot = document.querySelector('.guesses')
+const remaing = document.querySelector('.lastResult')
+const lowOrhi = document.querySelector('.lowOrhi')
+const startOver = document.querySelector('.resultParas')
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1 
+let playGame = true
+
+function validGuess(guess){
+    
+}
+
+function checkGuess(guess){
+
+}
+
+function displayGuess(huess){
+
+}
+
+function displayMsg(msg){
+
+}
